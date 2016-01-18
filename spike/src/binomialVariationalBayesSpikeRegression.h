dbcb9efd1d9dae42c8b957b7fe5f0a63dc60348d
Ben Logsdon <baelogsdon@gmail.com>
2016-01-18 15:04:04 -0800

    pushing forward on making C code for all Bayesian models with an underlying spike penalty more readable, robust, fascile, modular, etc...

--- spike/src/binomialVariationalBayesSpikeRegression.h
+++ spike/src/binomialVariationalBayesSpikeRegression.h
@@ -8,11 +8,11 @@
 #include <R_ext/BLAS.h>
 
 //generalizedLinearModelType: the type of genearlized linear model to fit
-typedef enum {NORMAL, BINOMIAL} generalizedLinearModelType;
+typedef enum {NORMAL, BINOMIAL, GAMMA, POISSON, MULTINOMIAL, INVERSEGAUSSIAN} generalizedLinearModelType;
 
 //modelSpaceSelection: whether to do Bayesian model averaging or
 //		  take the mode with maxmimum lower bound
-typedef enum {BMA, MAXIMAL, FULL} modelSpaceSelectionType;
+typedef enum {BMA,MAXIMAL,FULL} modelSpaceSelectionType;
 
 //X_mat: the data structure used to contain
 //	 a set of m vectors of length n
@@ -43,36 +43,36 @@ struct controlParameterSettings {
 	//l0Vector: a vector of the 'l0' penalty parameters to run along
 	const double * l0Vector;
 
-	//priorProbabilityVector: logistic transformation of l0Vector
-	//	   default: 1/(1+exp(-l0Vector))
+	//pb_path: logistic transformation of l0_path
+	//	   default: 1/(1+exp(-l0_path))
 	const double * priorProbabilityVector;
 
-	//penalizeVariable: an indicator vector which is 1 if a variable is penalized,
+	//exclude: an indicator vector which is 1 if a variable is penalized,
 	//         and 0 otherwise
 	//	   default: everything is 1 except intercept
 	const int * penalizeVariable;
 
-	//featureColumnScalingFactor: a vector of optional rescaling of predictors
+	//penalty_factor: a vector of optional rescaling of predictors
 	//		  default: everything is 1
 	const double * featureColumnScalingFactor;
 
-	//maximumIterations: maximum iterations to run
+	//maxit: maximum iterations to run
 	//	 default: 1e4
 	const int maximumIterations;
 
-	//l0VectorLength: length of path
+	//path_length: length of path
 	//	       default: 50
 	const int l0VectorLength;
 
-	//numberOfRealizations: number of restarts of algorithm
+	//n_orderings: number of restarts of algorithm
 	//	       default: 100
-	const int numberOfRealizations;
+	const int numberOfRandomRestarts;
 
 	//regressType: the type of regression used
 	//	       default: LINEAR
 	generalizedLinearModelType generalizedLinearModelUsed;
 
-	//scaleFeatureMatrix: whether or not to scale the columns
+	//scaleType: whether or not to scale the columns
 	//	     default: SCALE
 	const int scaleFeatureMatrix;
 
@@ -87,7 +87,7 @@ struct controlParameterSettings {
 	//		 default: APPR
 	const int applyBmaCovarianceCorrection;
 
-	//totalModelFits: l0VectorLength*numberOfRealizations
+	//total_replicates: path_length*n_orderings
 	const int totalModelFits;
 
 
@@ -155,26 +155,160 @@ struct gaussianLinkModelParameters {
 	//double v_sums_correct;
 	double betaSquaredExpectationCorrection;
 
+	//w_vec: the weights in the irls vbsr logistic regression
+	//	 algorithms
+	//	 default: 1
+	//double * w_vec;
+	double * irlsWeightVector;
+
+	//mu_vec: the pred values in the irls vbsr logistic
+	//	  regression algorithm
+	//        default: 0
+	//double * mu_vec;
+	double * irlsPredictionVector;
+
 	//resid_vec: the residual vector y-X%*%e_beta
 	//	     default: y
 	//double * resid_vec;
 	double * residualVector;
 
+	//pred_vec_old: the old prediction vector for irls vbsr
+	//	 	default: 0
+	//double * pred_vec_old;
+	double * irlsOldPredictionVector;
+
+	//pred_vec_new: the new prediction vector for irls vbsr
+	//		default: 0
+	//double * pred_vec_new;
+	double * irlsNewPredictionVector;
+
+	//x_w: the reweightings...
+	//	default: 0;
+	//double * x_w;
+	double * irlsReweightings;
+
+
 	//ord_index: the ordering of the current model_param
 	//	     default: 0
 	//int ord_index;
-	int realizationIndex;
+	int orderingIndex;
 
 	//path_index: the path index of the current model_param
 	//	      default: 0
 	//int path_index;
-	int penaltyIndex;
+	int penaltyPathIndex;
+
+
 };
 
+//struct for logistic regression parameters
+struct binomialLinkModelParameters {
+	//beta_mu: beta mean update from vbsr algorithm
+	//	   default: everything initialized to 0
+	//double * beta_mu;
+	double * betaMu;
+
+	//beta_sigma: beta variance update from vbsr algorithm
+	//	      default: everything initialized to 0
+	//double * beta_sigma;
+	double * betaSigmaSquared;
+
+	//beta_chi: betaMu/sqrt(betaSigmaSquared)
+	//	    default: 0
+	//double * beta_chi;
+	double * betaChi;
+
+	//beta_p: beta variational scaling update from vbsr algorithm
+	//	  default: everything initialized to 0
+	//double * beta_p;
+	double * betaPosteriorProbability;
+
+	//e_beta: expectation of beta from vbsr algorithm
+	//	  default: everything initalized to 0
+	//	  update: beta_p*beta_mu
+	//double * e_beta;
+	double * expectationBeta;
+
+	//e_beta_sq: expectation of beta_sq from vbsr algorithm
+	//	     default: everything is initialized to 0
+	//	     update: beta_p*(beta_mu^2+beta_sigma)
+	//double * e_beta_sq;
+  double * expectationBetaSquared;
+
+	//sigma_e: error variance update from vbsr algorithm
+	//	   default: initialized to variance of phenotype
+	//double sigma_e;
+	double sigmaSquaredError;
+
+	//lb: lower bound from vbsr algorithm
+	//    default: 0
+	//double lb;
+	double lowerBound;
+
+	//p_sums: sum of beta_p
+	//	  default: 0
+	//double p_sums;
+	double sumOfBetaPosteriorProbabilities;
+
+	//double entropy;
+	double posteriorProbabilityEntropy;
+
+	//v_sums_correct: a variable used to correct the lb
+	//		  expectation for the beta^2 terms
+	//		  sum_{j} (e_beta^2-e_beta_sq)*x_sum_sq
+	//		  default: 0
+	//double v_sums_correct;
+	double betaSquaredExpectationCorrection;
+
+	//w_vec: the weights in the irls vbsr logistic regression
+	//	 algorithms
+	//	 default: 1
+	//double * w_vec;
+	double * irlsWeightVector;
+
+	//mu_vec: the pred values in the irls vbsr logistic
+	//	  regression algorithm
+	//        default: 0
+	//double * mu_vec;
+	double * irlsPredictionVector;
+
+	//resid_vec: the residual vector y-X%*%e_beta
+	//	     default: y
+	//double * resid_vec;
+	double * residualVector;
+
+	//pred_vec_old: the old prediction vector for irls vbsr
+	//	 	default: 0
+	//double * pred_vec_old;
+	double * irlsOldPredictionVector;
+
+	//pred_vec_new: the new prediction vector for irls vbsr
+	//		default: 0
+	//double * pred_vec_new;
+	double * irlsNewPredictionVector;
+
+	//x_w: the reweightings...
+	//	default: 0;
+	//double * x_w;
+	double * irlsReweightings;
+
+
+	//ord_index: the ordering of the current model_param
+	//	     default: 0
+	//int ord_index;
+	int orderingIndex;
+
+	//path_index: the path index of the current model_param
+	//	      default: 0
+	//int path_index;
+	int penaltyPathIndex;
+
+
+};
 
 //data_struct: the data structure that contains
 //	       all of the fixed data vectors
-struct dataRealization {
+struct data {
 
 	//X: the data structure containing all of the
 	//   relevant variables.
@@ -204,7 +338,7 @@ struct dataRealization {
 
 	//ordering: a vector of vectors of the multiple orderings
 	//	    to be run by the algorithm
-	const struct integerColumnVector * realizationMatrix;
+	const struct integerColumnVector * orderingMatrix;
 
 	//one_vec: a vector of length n of ones
 	const double * onesVector;
@@ -237,28 +371,28 @@ struct gaussianModelRealization {
 
 
 	//data: the data for a given model
-	struct dataRealization data;
+	struct data dataRealization;
 
 	//*model_param: a pointer to a pointer for the model
 	//		 parameters for a given run of the
 	// 		 algorithm.  The model_params are
-	//		 indexed over the realizations first
+	//		 indexed over the orderings first
 	//		 then the l0 path.
 	struct gaussianLinkModelParameterRealization * modelParameterRealization;
 
 
 };
 
-//need to do a more explicit model comparison check
-void identifyUniqueModels(gaussianModelRealization * model);
+
+void identify_unique(double * lb_t, double * post_p, int numberSamples,double epsilon);
 
 double * extractPenalizedFeatureMatrixColumn(struct gaussianModelRealization * model, int columnIndex);
 
-int * extractOrderingMatrixolumn(struct gaussianModelRealization * model, int columnIndex);
+int * extractOrderingMatrixColumn(struct gaussianModelRealization * model, int columnIndex);
 
 struct gaussianModelRealization * getModelParameterRealization(struct gaussianModelRealization * model,int realizationIndex, int penaltyIndex);
 
-void initializeGaussianModelParameters(int numberSamples,
+void initialize_model_param(int numberSamples,
 				int numberPenalizedFeatures,
 				int realizationIndex,
 				int penaltyIndex,
@@ -266,50 +400,72 @@ void initializeGaussianModelParameters(int numberSamples,
 				double * responseVariable,
 				double responseVariance);
 
-void freeGaussianModelParameters(struct gaussianModelRealization * model, int realizationIndex, int penaltyIndex);
+void free_model_param(struct gaussianModelRealization * model, int realizationIndex, int penaltyIndex);
 
-void initializeGaussianModel(double * eps,
-			double * l0Vector,
-			double * priorProbabilityVector,
-			int * penalizeVariable,
-			double * featureColumnScalingFactor,
-			int * maximumIterations,
-			int * l0VectorLength,
-			int * numberOfRealizations,
+void initialize_model(double * eps,
+			double * l0_path,
+			double * pb_path,
+			int * exclude,
+			double * penalty_factor,
+			int * maxit,
+			int * path_length,
+			int * n_orderings,
 			int * regress,
 			int * scale,
 			int * est,
 			int * error,
 			double * kl,
-			int * applyBmaCovarianceCorrection,
-			int * totalModelFits,
-			double * penalizedDataMatrix,
-			double * responseVariable,
-			double * responseVariance,
-			int * numberSamples,
-			int * numberPenalizedFeatures,
+			int * approx,
+			int * total_replicates,
+			double * X,
+			double * y,
+			double * var_y,
+			int * n,
+			int * m,
 			int * ordering_mat,
 			struct gaussianModelRealization * model);
 
-void freeGaussianModel(struct gaussianModelRealization * model);
+void initialize_model_marg(double * eps,
+			int * exclude,
+			int * maxit,
+			int * regress,
+			int * scale,
+			double * X,
+			double * y,
+			double * var_y,
+			int * n,
+			int * m,
+			struct model_marg_struct * model);
 
-void dataStandardization(struct gaussianModelRealization * model);
+void free_model(struct gaussianModelRealization * model);
 
-void copyGaussianModelState(struct gaussianModelRealization * model, int i, int j);
+void free_model_marg(struct model_marg_struct * model);
 
-void updateGaussianBetaDistribution(struct gaussianModelRealization * model, int realizationIndex, int penaltyIndex);
+void process_data(struct gaussianModelRealization * model);
 
-void updateGaussianAlphaEstimate(struct gaussianModelRealization * model, int realizationIndex, int penaltyIndex);
+void process_data_marg(struct model_marg_struct * model);
 
-void updateGaussianSigmaSquaredErrorEstimate(struct gaussianModelRealization * model, int i, int j);
+void copy_model_state(struct gaussianModelRealization * model, int i, int j);
 
-void updateGaussianLowerBound(struct gaussianModelRealization * model, int i, int j);
+void update_beta(struct gaussianModelRealization * model, int i, int j);
 
-void runGaussianVariationalBayesSpikeRegression(struct gaussianModelRealization * model);
+void update_beta_marg(struct model_marg_struct * model, int * use_vec,int cv);
 
-void computeBayesianModelAveragingCovarianceCorrection(struct gaussianModelRealization * model,int k,double * post_prob,double * s_bma,int j);
+void update_error(struct gaussianModelRealization * model, int i, int j);
 
-void extractGaussianResults(struct gaussianModelRealization * model,
+void update_error_marg(struct model_marg_struct * model);
+
+void update_lb(struct gaussianModelRealization * model, int i, int j);
+
+void update_lb_marg(struct model_marg_struct * model);
+
+void run_vbsr(struct gaussianModelRealization * model);
+
+void run_marg(struct model_marg_struct * model);
+
+void compute_bma_correct(struct gaussianModelRealization * model,int k,double * post_prob,double * s_bma,int j);
+
+void collapse_results(struct gaussianModelRealization * model,
 			double * beta_chi_mat,
 			double * beta_mu_mat,
 			double * beta_sigma_mat,
@@ -318,21 +474,44 @@ void extractGaussianResults(struct gaussianModelRealization * model,
 			double * lb_mat,
 			double * kl_mat);
 
-void gaussianVariationalBayesSpikeRegression(double * eps,
-			double * l0Vector,
-			double * priorProbabilityVector,
-			int * penalizeVariable,
-			double * featureColumnScalingFactor,
-			int * maximumIterations,
-			int * l0VectorLength,
-			int * numberOfRealizations,
+void collapse_results_marg(struct model_marg_struct * model,
+			double * beta_chi,
+			double * beta_mu,
+			double * beta_sigma,
+			double * beta_p,
+			double * lb);
+
+void run_marg_analysis(double * eps,
+			int * exclude,
+			int * maxit,
+			int * regress,
+			int * scale,
+			double * X,
+			double * y,
+			double * var_y,
+			int * n,
+			int * m,
+			double * beta_chi,
+			double * beta_mu,
+			double * beta_sigma,
+			double * beta_p_mat,
+			double * lb);
+
+void run_vbsr_wrapper(double * eps,
+			double * l0_path,
+			double * pb_path,
+			int * exclude,
+			double * penalty_factor,
+			int * maxit,
+			int * path_length,
+			int * n_orderings,
 			int * regress,
 			int * scale,
 			int * est,
 			int * error,
 			double * kl,
 			int * approx,
-			int * totalModelFits,
+			int * total_replicates,
 			double * X,
 			double * y,
 			double * var_y,
